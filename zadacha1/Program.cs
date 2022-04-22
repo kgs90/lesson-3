84bb32fe570f427a0c8bd838efc684c6c9c3a4f3
kgs90 <kgs90@inbox.ru>
2022-04-22 22:08:39 +0300

    pervaya zadacha

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] x = new int [5];
+x[0] = 10;
+x[1] = 35;
+x[2] = 100;
+x[3] = 35;
+x[4] = 10;
+if (x[0] == x[4] && x[1] == x[3])
+{
+    System.Console.WriteLine("палиндром");
+}
+else
+{
+    System.Console.WriteLine("не палиндром");
+}
